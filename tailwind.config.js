9d794f4890e4a7891f86ec3bd8df4070d1d0a371
tina otarashvili <tinatinotarashvili99@gmail.com>
2023-12-04 03:29:56 +0400

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,36 +3,12 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      yes: {
-        backgroundColor: '#4fa954',
-        color: '#ffffff',
-        borderRadius: '0.25rem',
-        padding: '0 0.5rem',
-        textAlign: 'center',
-      },
-      no: {
-        backgroundColor: '#6b6b6b',
-        color: '#ffffff',
-        borderRadius: '0.25rem',
-        padding: '0 0.5rem',
-        textAlign: 'center'
- 
-      },
-      active : {
-        color:' #4788ba'
+    extend:
+    {
+       fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      }
     },
-    navText : {
-      color:' #808689'
-  },
-      animation: {
-        'infinite-scroll': 'infinite-scroll 25s linear infinite',
-      },
-      keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-      }                    
     },
   },
   plugins: [],
